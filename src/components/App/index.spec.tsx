fd4538033ad6c445062b1487084dd71f747830e1
Wiktor Jaszczuk <wiktor.jaszczuk@wffe.io>
2018-11-11 21:44:00 +0100

    rewrite tests using enzyme

--- src/components/App/index.spec.tsx
+++ src/components/App/index.spec.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
+import { shallow, ShallowWrapper } from 'enzyme';
+import 'jest-enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let sut: ShallowWrapper;
+
+  beforeEach(() => {
+    sut = shallow(<App />);
+  });
+
+  it('will match snapshot', () => {
+    expect(sut).toMatchSnapshot();
+  });
+
+  it('will contain image with classname app__logo', () => {
+    expect(sut.find('img')).toHaveClassName('app__logo');
+  });
 });

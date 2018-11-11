b46a0e93370994e2578b95843263eca2006915d4
Wiktor Jaszczuk <wiktor.jaszczuk@wffe.io>
2018-11-11 21:20:49 +0100

    install prettier, husky and lint-staged

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,22 +2,23 @@ import * as React from 'react';
 import logo from './logo.svg';
 import './index.css';
 
-const App: React.FunctionComponent = () =>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App: React.FunctionComponent = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
